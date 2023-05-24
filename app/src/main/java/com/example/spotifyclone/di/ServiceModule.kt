a964ca3a2a9136f0458a03519d713e3b132d8418
pog <nlaszanowski-97@wp.pl>
2023-05-24 12:12:46 +0200

    Initial commit

---
+++ app/src/main/java/com/example/spotifyclone/di/ServiceModule.kt
@@ -0,0 +1,54 @@
+package com.example.spotifyclone.di
+
+import android.content.Context
+import android.media.AudioAttributes
+import com.plcoding.spotifycloneyt.data.remote.MusicDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ServiceComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.android.scopes.ServiceScoped
+
+@Module
+@InstallIn(ServiceComponent::class)
+object ServiceModule {
+
+    @ServiceScoped
+    @Provides
+    fun provideMusicDatabase() = MusicDatabase()
+
+    @ServiceScoped
+    @Provides
+    fun provideAudioAttributes() = AudioAttributes.Builder()
+        .setContentType(C.CONTENT_TYPE_MUSIC)
+        .setUsage(C.USAGE_MEDIA)
+        .build()
+
+    @ServiceScoped
+    @Provides
+    fun provideExoPlayer(
+        @ApplicationContext context: Context,
+        audioAttributes: AudioAttributes
+    ) = SimpleExoPlayer.Builder(context).build().apply {
+        setAudioAttributes(audioAttributes, true)
+        setHandleAudioBecomingNoisy(true)
+    }
+
+    @ServiceScoped
+    @Provides
+    fun provideDataSourceFactory(
+        @ApplicationContext context: Context
+    ) = DefaultDataSourceFactory(context, Util.getUserAgent(context, "Spotify App"))
+}
+
+
+
+
+
+
+
+
+
+
+
